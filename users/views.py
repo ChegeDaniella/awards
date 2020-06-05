b5e9f7a0c78adce4e54728200cbcf24501055dc5
ChegeDaniella <chegedaniella@gmail>
2020-06-05 17:21:58 +0300

    updating user profile

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm,ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 def register(request):
@@ -17,5 +17,22 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')    
+    if request.method == 'POST':    
+        U_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+        if U_form.is_valid() and p_form.is_valid():
+            U_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated')
+            return redirect('profile')
+    else:
+        U_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+
+        context = {
+            'U_form':U_form,
+            'p_form':p_form
+        }
+    return render(request,'users/profile.html', context)    
 
