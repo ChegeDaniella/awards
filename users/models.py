d452705c4e79c0416574ead016940f91663903b6
ChegeDaniella <chegedaniella@gmail>
2020-06-05 19:22:13 +0300

    resizing profile image

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -8,4 +9,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+        if img.height > 300 or img.width>300:
+            output_size = (400,400)
+            img.thumbnail(output_size)
+            img.save(self.avatar.path)
+
+
 
