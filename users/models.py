752ab5788684ad8f2a571b14a1e10cb05bc56944
ChegeDaniella <chegedaniella@gmail>
2020-06-05 16:04:01 +0300

    adding profile table

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(default = 'default.jpg', upload_to='saved')
+    Bio = models.CharField(max_length = 500)
+
+    def __str__(self):
+        return f'{self.user} Profile'
 
